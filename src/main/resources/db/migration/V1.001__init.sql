8d182ecd39059fdb8ad8922485cec6ad4faf1e4b
Tejas Ozarkar <tejozarkar@gmail.com>
2020-06-24 00:20:56 +0530

    FEATURE: Flyway Added

---
+++ src/main/resources/db/migration/V1.001__init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE product(
+    id SERIAL PRIMARY KEY ,
+    name varchar(255),
+    price double precision
+);
---
+++ src/test/java/io/tej/FlywayDemo/FlywayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tej.FlywayDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
